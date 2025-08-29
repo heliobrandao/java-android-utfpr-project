199989148eea2cb4753367d722152ade7fc1adf2
heliobrandao <heliob79@gmail.com>
2025-08-29 17:42:28 -0300

    criacao arrays.xml

--- app/src/main/java/br/edu/utfpr/heliobrandao/viniveritas/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/heliobrandao/viniveritas/MainActivity.java
@@ -46,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         inicializarComponentes();
-        configurarSpinner();
         configurarBotoes();
     }
 
@@ -64,63 +63,26 @@ public class MainActivity extends AppCompatActivity {
         buttonSalvar = findViewById(R.id.buttonSalvar);
     }
 
-    private void configurarSpinner() {
-        String[] paises = {
-            "Selecione um país",
-            "Brasil",
-            "França",
-            "Itália",
-            "Espanha",
-            "Portugal",
-            "Argentina",
-            "Chile",
-            "Estados Unidos",
-            "Alemanha",
-            "Austrália"
-        };
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-            android.R.layout.simple_spinner_item, paises);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerPais.setAdapter(adapter);
-    }
-
     private void configurarBotoes() {
-        buttonLimpar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                limparFormulario();
-            }
-        });
-
-        buttonSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                salvarFormulario();
-            }
-        });
+        buttonLimpar.setOnClickListener(v -> limparFormulario());
+        buttonSalvar.setOnClickListener(v -> salvarFormulario());
     }
 
     private void limparFormulario() {
-        // Limpar EditTexts
         editTextNomeVinho.setText("");
         editTextSafra.setText("");
         editTextComentarios.setText("");
-
-        // Resetar Spinner para primeira posição
         spinnerPais.setSelection(0);
-
-        // Desmarcar RadioButtons
         radioGroupTipo.clearCheck();
-
-        // Desmarcar CheckBoxes
         checkBoxSeco.setChecked(false);
         checkBoxDoce.setChecked(false);
         checkBoxFrutado.setChecked(false);
         checkBoxTanico.setChecked(false);
 
-        // Mostrar Toast de confirmação
-        Toast.makeText(this, "Formulário limpo com sucesso!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(
+                this,
+                getString(R.string.formulario_limpo),
+                Toast.LENGTH_LONG).show();
     }
 
     private void salvarFormulario() {
@@ -128,25 +90,37 @@ public class MainActivity extends AppCompatActivity {
         String safra = editTextSafra.getText().toString().trim();
 
         if (nomeVinho.isEmpty()) {
-            Toast.makeText(this, "Por favor, informe o nome do vinho!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(
+                    this,
+                    getString(R.string.erro_nome_vazio),
+                    Toast.LENGTH_LONG).show();
             editTextNomeVinho.requestFocus();
             return;
         }
 
         if (safra.isEmpty()) {
-            Toast.makeText(this, "Por favor, informe a safra!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(
+                    this,
+                    getString(R.string.erro_safra_vazia),
+                    Toast.LENGTH_LONG).show();
             editTextSafra.requestFocus();
             return;
         }
 
         if (spinnerPais.getSelectedItemPosition() == 0) {
-            Toast.makeText(this, "Por favor, selecione um país!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(
+                    this,
+                    getString(R.string.erro_pais_nao_selecionado),
+                    Toast.LENGTH_LONG).show();
             return;
         }
 
         int radioSelecionado = radioGroupTipo.getCheckedRadioButtonId();
         if (radioSelecionado == -1) {
-            Toast.makeText(this, "Por favor, selecione o tipo de vinho!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(
+                    this,
+                    getString(R.string.erro_tipo_nao_selecionado),
+                    Toast.LENGTH_LONG).show();
             return;
         }
 
@@ -155,22 +129,23 @@ public class MainActivity extends AppCompatActivity {
         RadioButton radioButtonSelecionado = findViewById(radioSelecionado);
         String tipoVinho = radioButtonSelecionado.getText().toString();
 
+        //aqui criamos uma lista de caracteristicas utilizando ArrayList e checkboxes
         List<String> caracteristicas = new ArrayList<>();
-        if (checkBoxSeco.isChecked()) caracteristicas.add("Seco");
-        if (checkBoxDoce.isChecked()) caracteristicas.add("Doce");
-        if (checkBoxFrutado.isChecked()) caracteristicas.add("Frutado");
-        if (checkBoxTanico.isChecked()) caracteristicas.add("Tânico");
+        if (checkBoxSeco.isChecked()) caracteristicas.add(getString(R.string.caracteristica_seco));
+        if (checkBoxDoce.isChecked()) caracteristicas.add(getString(R.string.caracteristica_doce));
+        if (checkBoxFrutado.isChecked()) caracteristicas.add(getString(R.string.caracteristica_frutado));
+        if (checkBoxTanico.isChecked()) caracteristicas.add(getString(R.string.caracteristica_tanico));
 
         String comentarios = editTextComentarios.getText().toString().trim();
 
         StringBuilder mensagem = new StringBuilder();
-        mensagem.append("Vinho: ").append(nomeVinho).append("\n");
-        mensagem.append("Safra: ").append(safra).append("\n");
-        mensagem.append("País: ").append(paisSelecionado).append("\n");
-        mensagem.append("Tipo: ").append(tipoVinho);
+        mensagem.append(getString(R.string.vinho_dados_nome)).append(nomeVinho).append("\n");
+        mensagem.append(getString(R.string.vinho_dados_safra)).append(safra).append("\n");
+        mensagem.append(getString(R.string.vinho_dados_pais)).append(paisSelecionado).append("\n");
+        mensagem.append(getString(R.string.vinho_dados_tipo)).append(tipoVinho);
 
         if (!caracteristicas.isEmpty()) {
-            mensagem.append("\nCaracterísticas: ");
+            mensagem.append("\n").append(getString(R.string.vinho_dados_caracteristicas));
             for (int i = 0; i < caracteristicas.size(); i++) {
                 mensagem.append(caracteristicas.get(i));
                 if (i < caracteristicas.size() - 1) {
@@ -180,11 +155,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (!comentarios.isEmpty()) {
-            mensagem.append("\nComentários: ").append(comentarios);
+            mensagem.append("\n").append(getString(R.string.vinho_dados_comentarios)).append(comentarios);
         }
 
-        // Mostrar Toast com os dados salvos (limitado a 2 linhas)
-        Toast.makeText(this, "Vinho salvo: " + nomeVinho + " (" + tipoVinho + ")",
-                      Toast.LENGTH_LONG).show();
+        //aqui demos preferencia em apenas mostrar o nome do vinho e o tipo do vinho ao salvar
+        String toastMessage = getString(R.string.vinho_salvo_prefixo) + nomeVinho + " (" + tipoVinho + ")";
+        Toast.makeText(
+                this,
+                toastMessage,
+                Toast.LENGTH_LONG).show();
     }
 }
